315ee1431fef8c952bd027f3499073f0171319d4
Artem S <kisumoto.ru@gmail.com>
2018-01-23 14:20:47 +0300

    Do not duplicate kills in involved list
    Closes: #27

--- src/view-brcat.js
+++ src/view-brcat.js
@@ -517,7 +517,6 @@ function buildInvolved()
       {
         player.ships.forEach(( ship, shipIdx ) => {
           var invEntry = initInvolvedEntry( player );
-          invEntry.kills = ship.kills.length;
           teamLosses[ player.group.team ] += ship.lost;
           var temp = gShipTypes.find(X => X.I == ship.ship_type_id);
           if ( temp != undefined )
